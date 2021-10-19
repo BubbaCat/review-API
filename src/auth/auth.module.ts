09a46ae3868e7247a6d4d2ca8577866a0dbd5d50
Evgeniy <evgeniy.sapov@gmail.com>
2021-10-19 22:52:39 +0500

    Added auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,33 @@
+import { jwtStrategy } from './strategies/jwt.strategy';
+import { getJWTconfig } from './../config/jwt.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthModel } from './auth.model';
+import { UserModel } from './user.model';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
   imports: [
     TypegooseModule.forFeature([
       {
-        typegooseClass: AuthModel,
+        typegooseClass: UserModel,
          schemaOptions: {
-          collection: 'Auth'
+          collection: 'UserModel'
         }
       }
-    ])
-  ]
+    ]),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: getJWTconfig
+    }),
+    PassportModule
+  ],
+  providers: [AuthService,jwtStrategy]
 })
 export class AuthModule {}
